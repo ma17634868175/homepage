fc0e04f21cd878736bbad80d968463d7c8da4168
marx <marx@marx-de-MacBook-Pro.local>
2020-04-16 15:19:56 +0800

    [feat]bom style
    修改网页标题样式

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-15 22:42:20
+ * @LastEditTime: 2020-04-16 15:18:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /web-investment/Users/marx/Downloads/GitHub/flappy_bird/webpack.config.js
+ */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 
@@ -7,7 +15,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template:"public/index.html"
+            template:"public/index.html",
+            favicon: "public/favicon.ico"
         }),
         new CopyPlugin([
             { from: './src/assets', to: './assets' },
